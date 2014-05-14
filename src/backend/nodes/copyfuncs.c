07bfee6aea26a6e3827d60a30ba5cebcfc58f2e5
Thomas Munro <munro@ip9.org>
2014-05-14 20:13:18 +0000

    Updated node copying functions.
    I had forgotten to copy the skipLocked field, and this caused the SKIP
    LOCKED DATA not to work in pl/pgsql (presumably because nodes were
    incorrectly copied).

--- src/backend/nodes/copyfuncs.c
+++ src/backend/nodes/copyfuncs.c
@@ -960,6 +960,7 @@ _copyPlanRowMark(const PlanRowMark *from)
 	COPY_SCALAR_FIELD(rowmarkId);
 	COPY_SCALAR_FIELD(markType);
 	COPY_SCALAR_FIELD(noWait);
+	COPY_SCALAR_FIELD(skipLocked);
 	COPY_SCALAR_FIELD(isParent);
 
 	return newnode;
@@ -2071,6 +2072,7 @@ _copyRowMarkClause(const RowMarkClause *from)
 	COPY_SCALAR_FIELD(rti);
 	COPY_SCALAR_FIELD(strength);
 	COPY_SCALAR_FIELD(noWait);
+	COPY_SCALAR_FIELD(skipLocked);
 	COPY_SCALAR_FIELD(pushedDown);
 
 	return newnode;
@@ -2440,6 +2442,7 @@ _copyLockingClause(const LockingClause *from)
 	COPY_NODE_FIELD(lockedRels);
 	COPY_SCALAR_FIELD(strength);
 	COPY_SCALAR_FIELD(noWait);
+	COPY_SCALAR_FIELD(skipLocked);
 
 	return newnode;
 }
