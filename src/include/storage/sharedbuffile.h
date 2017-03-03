ef8b4c8d2ceec17c1f88a3349c56504c0bfa8bff
Thomas Munro <thomas.munro@enterprisedb.com>
2017-03-03 23:51:05 +1300

    Tidied

--- src/include/storage/sharedbuffile.h
+++ src/include/storage/sharedbuffile.h
@@ -3,7 +3,6 @@
  * sharedbuffile.h
  *	  Facilities for sharing temporary files between backends.
  *
- *
  * Portions Copyright (c) 1996-2017, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
  *
@@ -20,22 +19,22 @@ struct SharedBufFileSet;
 typedef struct SharedBufFileSet SharedBufFileSet;
 
 extern void SharedBufFileSetInitialize(SharedBufFileSet *set,
-									   int nparticipants,
+									   int participants,
 									   dsm_segment *segment);
-extern Size SharedBufFileSetSize(int nparticipants);
+extern Size SharedBufFileSetSize(int participants);
 extern void SharedBufFileSetAttach(SharedBufFileSet *set,
 								   dsm_segment *segment);
 
 extern BufFile *SharedBufFileCreate(SharedBufFileSet *set,
-									int file_number,
+									int partition,
 									int participant);
 extern void SharedBufFileDestroy(SharedBufFileSet *set,
-								 int file_number,
+								 int partition,
 								 int participant);
 extern void SharedBufFileExport(SharedBufFileSet *set,
 								BufFile *file);
 extern BufFile *SharedBufFileImport(SharedBufFileSet *set,
-									int file_number,
+									int partition,
 									int participant);
 
 #endif   /* SHAREDBUFFILE_H */
