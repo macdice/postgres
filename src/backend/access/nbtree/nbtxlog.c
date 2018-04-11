ddea06ee3e218f97e2e6db891507535ecd162534
Amit Kapila <amit.kapila@enterprisedb.com>
2018-04-11 16:12:12 +0530

    Retrieve the transaction slot of modified tuple
    This is required for the upcoming tuple locking patch.
    
    Patch by me, reviewed by Dilip Kumar

--- src/backend/access/nbtree/nbtxlog.c
+++ src/backend/access/nbtree/nbtxlog.c
@@ -642,7 +642,7 @@ btree_xlog_delete_get_latestRemovedXid(XLogReaderState *record)
 			ztup.t_len = ItemIdGetLength(hitemid);
 			ztup.t_tableOid = InvalidOid;
 			ztup.t_data = NULL;
-			ZHeapTupleGetTransInfo(&ztup, hbuffer, NULL, &xid, NULL, NULL,
+			ZHeapTupleGetTransInfo(&ztup, hbuffer, NULL, NULL, &xid, NULL, NULL,
 								   false);
 			if (TransactionIdDidCommit(xid) &&
 				TransactionIdFollows(xid, latestRemovedXid))
@@ -666,8 +666,8 @@ btree_xlog_delete_get_latestRemovedXid(XLogReaderState *record)
 				{
 					TransactionId	xid;
 
-					ZHeapTupleGetTransInfo(&ztup, hbuffer, NULL, &xid, NULL, NULL,
-										   false);
+					ZHeapTupleGetTransInfo(&ztup, hbuffer, NULL, NULL, &xid,
+										   NULL, NULL, false);
 					elog(DEBUG1, "TransactionId: %d",xid);
 					ZHeapTupleHeaderAdvanceLatestRemovedXid(ztuphdr, xid, &latestRemovedXid);
 				}
