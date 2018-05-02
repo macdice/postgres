macdice/postgres

commit 41c912cad15955b5f9270ef3688a44e91d410d3d

Clean up warnings from -Wimplicit-fallthrough.
Recent gcc can warn about switch-case fall throughs that are not
explicitly labeled as intentional.  This seems like a good thing,
so clean up the warnings exposed thereby by labeling all such
cases with comments that gcc will recognize.

In files that already had one or more suitable comments, I generally
matched the existing style of those.  Otherwise I went with
/* FALLTHROUGH */, which is one of the spellings approved at the
more-restrictive-than-default level -Wimplicit-fallthrough=4.
(At the default level you can also spell it /* FALL ?THRU */,
and it's not picky about case.  What you can't do is include
additional text in the same comment, so some existing comments
containing versions of this aren't good enough.)

Testing with gcc 8.0.1 (Fedora 28's current version), I found that
I also had to put explicit "break"s after elog(ERROR) or ereport(ERROR);
apparently, for this purpose gcc doesn't recognize that those don't
return.  That seems like possibly a gcc bug, but it's fine because
in most places we did that anyway; so this amounts to a visit from the
style police.

Discussion: https://postgr.es/m/15083.1525207729@sss.pgh.pa.us

src/bin/pgbench/pgbench.c
@@ -1978,6 +1978,8 @@ evalStandardFunc(TState *thread, CState *st,
 							Assert(0);
 					}
 				}
+
+				break;			/* NOTREACHED */
 			}
 
 			/* integer bitwise operators */

contrib/btree_gin/btree_gin.c
@@ -88,6 +88,7 @@ gin_btree_extract_query(FunctionCallInfo fcinfo,
 		case BTGreaterEqualStrategyNumber:
 		case BTGreaterStrategyNumber:
 			*ptr_partialmatch = true;
+			/* FALLTHROUGH */
 		case BTEqualStrategyNumber:
 			entries[0] = datum;
 			break;

contrib/pageinspect/hashfuncs.c
@@ -97,18 +97,22 @@ verify_hash_page(bytea *raw_page, int flags)
 				ereport(ERROR,
 						(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 						 errmsg("page is not a hash meta page")));
+				break;
 			case LH_BUCKET_PAGE | LH_OVERFLOW_PAGE:
 				ereport(ERROR,
 						(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 						 errmsg("page is not a hash bucket or overflow page")));
+				break;
 			case LH_OVERFLOW_PAGE:
 				ereport(ERROR,
 						(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 						 errmsg("page is not a hash overflow page")));
+				break;
 			default:
 				elog(ERROR,
 					 "hash page of type %08x not in mask %08x",
 					 pagetype, flags);
+				break;
 		}
 	}
 

src/backend/access/hash/hashfunc.c
@@ -466,9 +466,9 @@ hash_any(register const unsigned char *k, register int keylen)
 				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 24);
-				/* the lowest byte of c is reserved for the length */
 				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += ka[1];
 				a += ka[0];
 				break;
@@ -505,9 +505,9 @@ hash_any(register const unsigned char *k, register int keylen)
 				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 8);
-				/* the lowest byte of c is reserved for the length */
 				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += ka[1];
 				a += ka[0];
 				break;
@@ -558,57 +558,77 @@ hash_any(register const unsigned char *k, register int keylen)
 
 		/* handle the last 11 bytes */
 #ifdef WORDS_BIGENDIAN
-		switch (len)			/* all the case statements fall through */
+		switch (len)
 		{
 			case 11:
 				c += ((uint32) k[10] << 8);
+				/* fall through */
 			case 10:
 				c += ((uint32) k[9] << 16);
+				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 24);
-				/* the lowest byte of c is reserved for the length */
+				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += k[7];
+				/* fall through */
 			case 7:
 				b += ((uint32) k[6] << 8);
+				/* fall through */
 			case 6:
 				b += ((uint32) k[5] << 16);
+				/* fall through */
 			case 5:
 				b += ((uint32) k[4] << 24);
+				/* fall through */
 			case 4:
 				a += k[3];
+				/* fall through */
 			case 3:
 				a += ((uint32) k[2] << 8);
+				/* fall through */
 			case 2:
 				a += ((uint32) k[1] << 16);
+				/* fall through */
 			case 1:
 				a += ((uint32) k[0] << 24);
 				/* case 0: nothing left to add */
 		}
 #else							/* !WORDS_BIGENDIAN */
-		switch (len)			/* all the case statements fall through */
+		switch (len)
 		{
 			case 11:
 				c += ((uint32) k[10] << 24);
+				/* fall through */
 			case 10:
 				c += ((uint32) k[9] << 16);
+				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 8);
-				/* the lowest byte of c is reserved for the length */
+				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += ((uint32) k[7] << 24);
+				/* fall through */
 			case 7:
 				b += ((uint32) k[6] << 16);
+				/* fall through */
 			case 6:
 				b += ((uint32) k[5] << 8);
+				/* fall through */
 			case 5:
 				b += k[4];
+				/* fall through */
 			case 4:
 				a += ((uint32) k[3] << 24);
+				/* fall through */
 			case 3:
 				a += ((uint32) k[2] << 16);
+				/* fall through */
 			case 2:
 				a += ((uint32) k[1] << 8);
+				/* fall through */
 			case 1:
 				a += k[0];
 				/* case 0: nothing left to add */
@@ -686,9 +706,9 @@ hash_any_extended(register const unsigned char *k, register int keylen,
 				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 24);
-				/* the lowest byte of c is reserved for the length */
 				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += ka[1];
 				a += ka[0];
 				break;
@@ -725,9 +745,9 @@ hash_any_extended(register const unsigned char *k, register int keylen,
 				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 8);
-				/* the lowest byte of c is reserved for the length */
 				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += ka[1];
 				a += ka[0];
 				break;
@@ -778,57 +798,77 @@ hash_any_extended(register const unsigned char *k, register int keylen,
 
 		/* handle the last 11 bytes */
 #ifdef WORDS_BIGENDIAN
-		switch (len)			/* all the case statements fall through */
+		switch (len)
 		{
 			case 11:
 				c += ((uint32) k[10] << 8);
+				/* fall through */
 			case 10:
 				c += ((uint32) k[9] << 16);
+				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 24);
-				/* the lowest byte of c is reserved for the length */
+				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += k[7];
+				/* fall through */
 			case 7:
 				b += ((uint32) k[6] << 8);
+				/* fall through */
 			case 6:
 				b += ((uint32) k[5] << 16);
+				/* fall through */
 			case 5:
 				b += ((uint32) k[4] << 24);
+				/* fall through */
 			case 4:
 				a += k[3];
+				/* fall through */
 			case 3:
 				a += ((uint32) k[2] << 8);
+				/* fall through */
 			case 2:
 				a += ((uint32) k[1] << 16);
+				/* fall through */
 			case 1:
 				a += ((uint32) k[0] << 24);
 				/* case 0: nothing left to add */
 		}
 #else							/* !WORDS_BIGENDIAN */
-		switch (len)			/* all the case statements fall through */
+		switch (len)
 		{
 			case 11:
 				c += ((uint32) k[10] << 24);
+				/* fall through */
 			case 10:
 				c += ((uint32) k[9] << 16);
+				/* fall through */
 			case 9:
 				c += ((uint32) k[8] << 8);
-				/* the lowest byte of c is reserved for the length */
+				/* fall through */
 			case 8:
+				/* the lowest byte of c is reserved for the length */
 				b += ((uint32) k[7] << 24);
+				/* fall through */
 			case 7:
 				b += ((uint32) k[6] << 16);
+				/* fall through */
 			case 6:
 				b += ((uint32) k[5] << 8);
+				/* fall through */
 			case 5:
 				b += k[4];
+				/* fall through */
 			case 4:
 				a += ((uint32) k[3] << 24);
+				/* fall through */
 			case 3:
 				a += ((uint32) k[2] << 16);
+				/* fall through */
 			case 2:
 				a += ((uint32) k[1] << 8);
+				/* fall through */
 			case 1:
 				a += k[0];
 				/* case 0: nothing left to add */

src/backend/catalog/objectaddress.c
@@ -2092,6 +2092,7 @@ pg_get_object_address(PG_FUNCTION_ARGS)
 						(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
 						 errmsg("name list length must be at least %d", 3)));
 			/* fall through to check args length */
+			/* FALLTHROUGH */
 		case OBJECT_OPERATOR:
 			if (list_length(args) != 2)
 				ereport(ERROR,

src/backend/commands/explain.c
@@ -1479,7 +1479,8 @@ ExplainNode(PlanState *planstate, List *ancestors,
 		case T_SampleScan:
 			show_tablesample(((SampleScan *) plan)->tablesample,
 							 planstate, ancestors, es);
-			/* FALL THRU to print additional fields the same as SeqScan */
+			/* fall through to print additional fields the same as SeqScan */
+			/* FALLTHROUGH */
 		case T_SeqScan:
 		case T_ValuesScan:
 		case T_CteScan:

src/backend/commands/indexcmds.c
@@ -440,6 +440,7 @@ DefineIndex(Oid relationId,
 					(errcode(ERRCODE_WRONG_OBJECT_TYPE),
 					 errmsg("cannot create index on foreign table \"%s\"",
 							RelationGetRelationName(rel))));
+			break;
 		default:
 			ereport(ERROR,
 					(errcode(ERRCODE_WRONG_OBJECT_TYPE),

src/backend/commands/trigger.c
@@ -3338,6 +3338,7 @@ ltrmark:;
 
 			case HeapTupleInvisible:
 				elog(ERROR, "attempted to lock invisible tuple");
+				break;
 
 			default:
 				ReleaseBuffer(buffer);

commit 0cc95b30607407919177ab17691af14a01c99162

Fix compiler warning on Windows.
Commit 41c912ca caused MSVC to complain that not all control paths return
a value.

Thomas Munro

src/bin/pgbench/pgbench.c
@@ -1980,7 +1980,7 @@ evalStandardFunc(TState *thread, CState *st,
 					}
 				}
 
-				break;			/* NOTREACHED */
+				return false;		/* NOTREACHED */
 			}
 
 			/* integer bitwise operators */