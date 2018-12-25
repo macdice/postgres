1d3d78b7bcf1e2be939e8a5b9cc09e5b9f679e2b
Thomas Munro <thomas.munro@enterprisedb.com>
2018-12-26 12:03:51 +1300

    Add shared_memory_type GUC.
    Since 9.3 we have used anonymous shared mmap for our main shmem
    segment, except in EXEC_BACKEND builds.  Provide a GUC so that users
    can opt into System V shared memory once again.  This is reported to
    be useful at least on AIX systems.  Future commits will add huge/large
    page support for System V memory.
    
    Author: Andres Freund (revived and documented by Thomas Munro)
    Discussion: https://postgr.es/m/HE1PR0202MB28126DB4E0B6621CC6A1A91286D90%40HE1PR0202MB2812.eurprd02.prod.outlook.com
    Discussion: https://postgr.es/m/2AE143D2-87D3-4AD1-AC78-CE2258230C05%40FreeBSD.org

--- src/include/storage/pg_shmem.h
+++ src/include/storage/pg_shmem.h
@@ -52,12 +52,32 @@ typedef enum
 	HUGE_PAGES_TRY
 }			HugePagesType;
 
+/* Possible values for shared_memory_type */
+typedef enum
+{
+	SHMEM_TYPE_WINDOWS,
+	SHMEM_TYPE_SYSV,
+	SHMEM_TYPE_MMAP
+}			PGShmemType;
+
+/* GUC variables */
+extern int shared_memory_type;
+extern int huge_pages;
+
 #ifndef WIN32
 extern unsigned long UsedShmemSegID;
 #else
 extern HANDLE UsedShmemSegID;
 #endif
 extern void *UsedShmemSegAddr;
+
+#if !defined(WIN32) && !defined(EXEC_BACKEND)
+#define DEFAULT_SHARED_MEMORY_TYPE SHMEM_TYPE_MMAP
+#elif !defined(WIN32)
+#define DEFAULT_SHARED_MEMORY_TYPE SHMEM_TYPE_SYSV
+#else
+#define DEFAULT_SHARED_MEMORY_TYPE SHMEM_TYPE_WINDOWS
+#endif
 
 #ifdef EXEC_BACKEND
 extern void PGSharedMemoryReAttach(void);
