b83779a14d4fe7cd2ceee1ca0119c2a8c6d11820
Thomas Munro <thomas.munro@gmail.com>
2019-03-06 16:46:04 +1300

    Add undo log manager.
    Add a new subsystem to manage undo logs.  Undo logs allow data to be appended
    efficiently, like logs.  They also allow data to be discarded efficiently from
    the other end, like a queue.  Thirdly, they allow efficient buffered random
    access, like a relation.
    
    Undo logs physically consist of a set of 1MB segment files under
    $PGDATA/base/undo (or per-tablespace equivalent) that are created, deleted or
    renamed as required, similarly to the way that WAL segments are managed.
    Meta-data about the set of undo logs is stored in shared memory, and written
    to per-checkpoint files under $PGDATA/pg_undo.
    
    Provide access to the undo files managed by undolog.c through bufmgr.c.
    A new SMGR implementation allows bufmgr.c to access files created by
    undolog.c.
    
    Author: Thomas Munro, with contributions from Dilip Kumar, Rafia Sabih,
            Robert Haas and Amit Kapila
    Reviewed-by:
    Discussion: https://postgr.es/m/CAEepm%3D2EqROYJ_xYz4v5kfr4b0qw_Lq_6Pe8RTEC8rx3upWsSQ%40mail.gmail.com

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -31,6 +31,7 @@
 #include "access/transam.h"
 #include "access/tuptoaster.h"
 #include "access/twophase.h"
+#include "access/undolog.h"
 #include "access/xact.h"
 #include "access/xlog_internal.h"
 #include "access/xloginsert.h"
@@ -6712,6 +6713,9 @@ StartupXLOG(void)
 	 */
 	restoreTwoPhaseData();
 
+	/* Recover undo log meta data corresponding to this checkpoint. */
+	StartupUndoLogs(ControlFile->checkPointCopy.redo);
+
 	lastFullPageWrites = checkPoint.fullPageWrites;
 
 	RedoRecPtr = XLogCtl->RedoRecPtr = XLogCtl->Insert.RedoRecPtr = checkPoint.redo;
@@ -8979,6 +8983,7 @@ static void
 CheckPointGuts(XLogRecPtr checkPointRedo, int flags)
 {
 	CheckPointCLOG();
+	CheckPointUndoLogs(checkPointRedo, ControlFile->checkPointCopy.redo);
 	CheckPointCommitTs();
 	CheckPointSUBTRANS();
 	CheckPointMultiXact();
