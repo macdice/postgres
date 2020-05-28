4c7e293b3b8854b3383fdf76192e7f6b2160deac
Andres Freund <andres@anarazel.de>
2020-05-28 14:51:58 -0700

    Prefetch memory cache lines during hot pruning.
    Author: Andres Freund <andres@anarazel.de>
    Author: Dmitry Dolgov <9erthalion6@gmail.com>

--- src/backend/access/heap/pruneheap.c
+++ src/backend/access/heap/pruneheap.c
@@ -254,8 +254,31 @@ heap_page_prune(Relation relation, Buffer buffer,
 	prstate.nredirected = prstate.ndead = prstate.nunused = 0;
 	memset(prstate.marked, 0, sizeof(prstate.marked));
 
-	/* Scan the page */
 	maxoff = PageGetMaxOffsetNumber(page);
+
+	/* Prefetch cache lines. */
+	for (char *p = (char *) PageGetItemId(page, FirstOffsetNumber);
+		 p < (char *) PageGetItemId(page, maxoff);
+		 p += 64)
+	{
+		pg_prefetch_mem((ItemId)p);
+	}
+
+	for (offnum = FirstOffsetNumber;
+		 offnum <= maxoff;
+		 offnum = OffsetNumberNext(offnum))
+	{
+		ItemId		itemid;
+
+		itemid = PageGetItemId(page, offnum);
+		if (!ItemIdIsUsed(itemid) || ItemIdIsDead(itemid) || !ItemIdHasStorage(itemid))
+			continue;
+
+		pg_prefetch_mem((HeapTupleHeader) PageGetItem(page, itemid));
+		pg_prefetch_mem(PageGetItem(page, itemid) + sizeof(HeapTupleHeaderData) - 1);
+	}
+
+	/* Scan the page */
 	for (offnum = FirstOffsetNumber;
 		 offnum <= maxoff;
 		 offnum = OffsetNumberNext(offnum))
