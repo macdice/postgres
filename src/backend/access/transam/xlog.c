2f6d690cefc0cad8cbd8b88dbed4d688399c6916
Thomas Munro <thomas.munro@gmail.com>
2020-09-14 23:20:55 +1200

    WIP: Avoid extra buffer lookup when prefetching WAL blocks.
    Provide a some workspace in decoded WAL records, so that we can remember
    which buffer recently contained we found a block cached in, for later
    use when replaying the record.  Provide a new way to look up a
    recently-known buffer and check if it's still valid and has the right
    tag.
    
    XXX Needs review to figure out if it's safe or steamrolling over subtleties

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -1463,7 +1463,7 @@ checkXLogConsistency(XLogReaderState *record)
 		 * temporary page.
 		 */
 		buf = XLogReadBufferExtended(rnode, forknum, blkno,
-									 RBM_NORMAL_NO_LOG);
+									 RBM_NORMAL_NO_LOG, InvalidBuffer);
 		if (!BufferIsValid(buf))
 			continue;
 
