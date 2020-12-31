61a64bc26d1c7dd8a928eb823e2dcbc15903ba62
Andres Freund <andres@anarazel.de>
2020-12-30 20:21:49 -0800

    wip: wal: async commit: reduce frequency of latch sets.

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -686,6 +686,7 @@ typedef struct XLogCtlData
 	XLogRecPtr	RedoRecPtr;		/* a recent copy of Insert->RedoRecPtr */
 	FullTransactionId ckptFullXid;	/* nextXid of latest checkpoint */
 	XLogRecPtr	asyncXactLSN;	/* LSN of newest async commit/abort */
+	XLogRecPtr	asyncXactNotifiedLSN;	/*  */
 	XLogRecPtr	replicationSlotMinLSN;	/* oldest LSN needed by any slot */
 
 	XLogSegNo	lastRemovedSegNo;	/* latest removed/recycled XLOG segment */
@@ -3954,6 +3955,7 @@ void
 XLogSetAsyncXactLSN(XLogRecPtr asyncXactLSN)
 {
 	XLogRecPtr	WriteRqstPtr = asyncXactLSN;
+	XLogRecPtr	LastNotifiedLSN;
 	bool		sleeping;
 
 	SpinLockAcquire(&XLogCtl->info_lck);
@@ -3961,6 +3963,7 @@ XLogSetAsyncXactLSN(XLogRecPtr asyncXactLSN)
 	sleeping = XLogCtl->WalWriterSleeping;
 	if (XLogCtl->asyncXactLSN < asyncXactLSN)
 		XLogCtl->asyncXactLSN = asyncXactLSN;
+	LastNotifiedLSN = XLogCtl->asyncXactNotifiedLSN;
 	SpinLockRelease(&XLogCtl->info_lck);
 
 	/*
@@ -3974,8 +3977,24 @@ XLogSetAsyncXactLSN(XLogRecPtr asyncXactLSN)
 		WriteRqstPtr -= WriteRqstPtr % XLOG_BLCKSZ;
 
 		/* if we have already flushed that far, we're done */
-		if (WriteRqstPtr <= LogwrtResult.FlushDone)
+		if (WriteRqstPtr <= LogwrtResult.FlushInit)
 			return;
+
+		if (WriteRqstPtr <= LogwrtResult.FlushInit + WalWriterFlushAfter * XLOG_BLCKSZ)
+			return;
+
+		if (WriteRqstPtr <= LastNotifiedLSN)
+			return;
+#if 1
+		SpinLockAcquire(&XLogCtl->info_lck);
+		LastNotifiedLSN = XLogCtl->asyncXactNotifiedLSN;
+		if (LastNotifiedLSN < WriteRqstPtr)
+			XLogCtl->asyncXactNotifiedLSN = WriteRqstPtr;
+		SpinLockRelease(&XLogCtl->info_lck);
+
+		if (WriteRqstPtr <= LastNotifiedLSN)
+			return;
+#endif
 	}
 
 	/*
