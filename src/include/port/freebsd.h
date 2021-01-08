4be4e68d22f659320f71803029b7a2f901b40ed8
Thomas Munro <thomas.munro@gmail.com>
2021-01-08 14:06:27 +1300

    Default to wal_sync_method=fdatasync on FreeBSD.
    FreeBSD 13 gained O_DSYNC, which would normally cause wal_sync_method's
    default value to change to open_datasync according to the rules in
    xlogdefs.h.  That doesn't perform better than fdatasync on all
    workloads.  Let's keep fdatasync as the default for now.  Similar to
    commit 576477e73c4 for Linux.
    
    Back-patch to all releases.
    
    Discussion: https://postgr.es/m/CA+hUKGLsAMXBQrCxCXoW-JsUYmdOL8ALYvaX=CrHqWxm-nWbGA@mail.gmail.com

--- src/include/port/freebsd.h
+++ src/include/port/freebsd.h
@@ -1,1 +1,10 @@
 /* src/include/port/freebsd.h */
+
+/*
+ * Set the default wal_sync_method to fdatasync, because xlogdefs.h's rules
+ * would otherwise prefer open_datasync on FreeBSD 13+.  That isn't a good
+ * default for all workloads and file systems.
+ */
+#ifdef HAVE_FDATASYNC
+#define PLATFORM_DEFAULT_SYNC_METHOD	SYNC_METHOD_FDATASYNC
+#endif
