9fc77e6a02fb14118cd068f095f7438496792972
Thomas Munro <thomas.munro@gmail.com>
2021-11-09 16:33:10 +1300

    Add circular WAL decoding buffer, take II.
    Teach xlogreader.c to decode its output into a circular buffer, to
    support upcoming optimizations based on looking ahead.
    
     * XLogReadRecord() works as before, consuming records one by one, and
       allowing them to be examined via the traditional XLogRecGetXXX()
       macros, and the traditional members like xlogreader->ReadRecPtr.
    
     * An alternative new interface XLogReadAhead()/XLogNextRecord() is
       added that returns pointers to DecodedXLogRecord
       objects so that it's possible to look ahead in the WAL stream.
    
     * In order to be able to use the new interface effectively, client
       code should provide a page_read() callback that response to
       a new nonblocking mode by returning XLREAD_WOULDBLOCK to avoid
       waiting.  No such implementation is included in this commit,
       and other code that is unaware of the new mechanism doesn't need
       to change.
    
    The buffer's size can be set by the client of xlogreader.c.  Large
    records that don't fit in the circular buffer are called "oversized" and
    allocated separately with palloc().
    
    Discussion: https://postgr.es/m/CA+hUKGJ4VJN8ttxScUFM8dOKX0BrBiboo5uz1cq=AovOddfHpA@mail.gmail.com

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -1621,7 +1621,7 @@ checkXLogConsistency(XLogReaderState *record)
 
 	Assert((XLogRecGetInfo(record) & XLR_CHECK_CONSISTENCY) != 0);
 
-	for (block_id = 0; block_id <= record->max_block_id; block_id++)
+	for (block_id = 0; block_id <= XLogRecMaxBlockId(record); block_id++)
 	{
 		Buffer		buf;
 		Page		page;
@@ -11895,7 +11895,7 @@ xlog_redo(XLogReaderState *record)
 		 * resource manager needs to generate conflicts, it has to define a
 		 * separate WAL record type and redo routine.
 		 */
-		for (uint8 block_id = 0; block_id <= record->max_block_id; block_id++)
+		for (uint8 block_id = 0; block_id <= XLogRecMaxBlockId(record); block_id++)
 		{
 			Buffer		buffer;
 
@@ -12037,7 +12037,7 @@ xlog_block_info(StringInfo buf, XLogReaderState *record)
 	int			block_id;
 
 	/* decode block references */
-	for (block_id = 0; block_id <= record->max_block_id; block_id++)
+	for (block_id = 0; block_id <= XLogRecMaxBlockId(record); block_id++)
 	{
 		RelFileNode rnode;
 		ForkNumber	forknum;
