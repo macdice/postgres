ce9b29b7312486e4c68093c832119aec1fe0a6ba
Thomas Munro <thomas.munro@gmail.com>
2023-08-27 11:12:20 +1200

    fixup
    ci-os-only: freebsd

--- src/tools/ci/gcp_freebsd_repartition.sh
+++ src/tools/ci/gcp_freebsd_repartition.sh
@@ -15,13 +15,15 @@ gpart show da0
 swapoff -a || true
 # (apparently we can only have 4!?)
 gpart delete -i 3 da0
-gpart add -t freebsd-ufs -l data8k -a 4096 da0
+gpart add -t freebsd-zfs -l data8k -a 4096 da0
 gpart show da0
 zpool create tank /dev/da0p3
 zfs create tank/work
 zfs set atime=off tank/work
 zfs set compression=off tank/work
 zfs set recordsize=8192 tank/work
+sysctl vfs.zfs.nocacheflush=1
+#sysctl vfs.zfs.txg.timeout=1000
 
 # Migrate working directory
 du -hs $CIRRUS_WORKING_DIR
