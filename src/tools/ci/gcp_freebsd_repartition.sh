8b76a1d9b3fb3c694ee0956c2fcd94a5e3e4fec5
Thomas Munro <thomas.munro@gmail.com>
2023-08-27 11:01:24 +1200

    ci: Improve file system performance on FreeBSD.
    Previously we used a UFS partition on a regular disk partition, but if
    we put it on a memory device we can avoid a lot of I/O.
    
    ci-os-only: freebsd

--- src/tools/ci/gcp_freebsd_repartition.sh
+++ src/tools/ci/gcp_freebsd_repartition.sh
@@ -3,8 +3,7 @@
 set -e
 set -x
 
-# The default filesystem on freebsd gcp images is very slow to run tests on,
-# due to its 32KB block size
+# Set up a ZFS partition that is tuned to minimize I/O.
 #
 # XXX: It'd probably better to fix this in the image, using something like
 # https://people.freebsd.org/~lidl/blog/re-root.html
@@ -12,17 +11,22 @@ set -x
 # fix backup partition table after resize
 gpart recover da0
 gpart show da0
-# kill swap, so we can delete a partition
-swapoff -a || true
-# (apparently we can only have 4!?)
+
+# delete and re-add swap partition with expanded size
+swapoff -a
 gpart delete -i 3 da0
-gpart add -t freebsd-ufs -l data8k -a 4096 da0
+gpart add -t freebsd-swap -l swapfs -a 4096 da0
 gpart show da0
-newfs -U -b 8192 /dev/da0p3
+swapon -a
+
+# create a file system on a memory disk backed by swap, resulting in an ffs
+# file system that writes back more lazily
+mdconfig -a -t swap -s20G -u md1
+newfs -b 8192 -U /dev/md1
 
 # Migrate working directory
 du -hs $CIRRUS_WORKING_DIR
 mv $CIRRUS_WORKING_DIR $CIRRUS_WORKING_DIR.orig
 mkdir $CIRRUS_WORKING_DIR
-mount -o noatime /dev/da0p3 $CIRRUS_WORKING_DIR
+mount -o noatime /dev/md1 $CIRRUS_WORKING_DIR
 cp -r $CIRRUS_WORKING_DIR.orig/* $CIRRUS_WORKING_DIR/
