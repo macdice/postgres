b29cb00c8f4a1345deae3374b5bd04f8ba409073
Thomas Munro <thomas.munro@gmail.com>
2025-01-21 08:08:08 +1300

    Remove arbitrary cap on read stream buffer queue.
    Previously the read stream buffer queue was arbitrarily capped in size
    at max_ios * 4 or at least io_combine_limit based on defunct theories
    about sequential streams not needing to look far ahead until we get true
    AIO.  It was already not enough now, and could limit I/O concurrency
    that is needed for new proposed patches.
    
    The new number is (max_ios + 1) * io_combine_limit, which is enough for
    the full configured I/O concurrency level using the full configured I/O
    size, plus the buffers from one full-sized I/O that has just completed.
    
    As of the time of writing, the maximum GUC values for
    effective_io_concurrent (1000) and io_combine_limit (32) imply a queue
    with around 32K entries, though those numbers are likely to change.
    That requires a wider type in one place that has a intermediate value
    that might overflow before clamping.
    
    Discussion: https://postgr.es/m/CA%2BhUKGK_%3D4CVmMHvsHjOVrK6t4F%3DLBpFzsrr3R%2BaJYN8kcTfWg%40mail.gmail.com

--- src/backend/storage/aio/read_stream.c
+++ src/backend/storage/aio/read_stream.c
@@ -447,13 +447,15 @@ read_stream_begin_impl(int flags,
 
 	/*
 	 * Choose the maximum number of buffers we're prepared to pin.  We try to
-	 * pin fewer if we can, though.  We clamp it to at least io_combine_limit
-	 * so that we can have a chance to build up a full io_combine_limit sized
-	 * read, even when max_ios is zero.  Be careful not to allow int16 to
-	 * overflow (even though that's not possible with the current GUC range
-	 * limits), allowing also for the spare entry and the overflow space.
+	 * pin fewer if we can, though.  We add one so that we can make progress
+	 * even if max_ios is set to 0, and this also allows some extra space for
+	 * pinned buffers ready to be consumed after a wide I/O finishes.
+	 *
+	 * Be careful not to allow int16 to overflow (even though that's not
+	 * possible with the current GUC range limits), allowing also for the
+	 * spare entry and the overflow space.
 	 */
-	max_pinned_buffers = Max(max_ios * 4, io_combine_limit);
+	max_pinned_buffers = (max_ios + 1) * io_combine_limit;
 	max_pinned_buffers = Min(max_pinned_buffers,
 							 PG_INT16_MAX - io_combine_limit - 1);
 
@@ -725,7 +727,7 @@ read_stream_next_buffer(ReadStream *stream, void **per_buffer_data)
 		stream->ios[stream->oldest_io_index].buffer_index == oldest_buffer_index)
 	{
 		int16		io_index = stream->oldest_io_index;
-		int16		distance;
+		int32		distance;	/* wider temporary value, clamped below */
 
 		/* Sanity check that we still agree on the buffers. */
 		Assert(stream->ios[io_index].op.buffers ==
