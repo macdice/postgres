814735818caac750f372a7d205f009e8ad6ca9f1
Thomas Munro <thomas.munro@gmail.com>
2025-02-27 02:57:31 +1300

    Handle hit/miss patterns better in read stream.
    The previous algorithm would double ("attack") the look-ahead distance
    for misses, and "decay" by one for each hit.  This didn't work too well
    for a repeating hit-miss-hit-miss pattern, where we cloud clearly
    benefit from I/O concurrency but we'd be stuck with a distance of 1.
    Introduce a new counter "sustain", set to 16 when performing an I/O, and
    decremented by hits.  Only when it is zero does the distance begin to
    decay.  This helps boost us out of that pattern.
    
    XXX Better ideas?
    XXX Need forwarding patches too or the seq detector is confused by the
    secret hit...

--- src/backend/storage/aio/read_stream.c
+++ src/backend/storage/aio/read_stream.c
@@ -96,6 +96,7 @@ struct ReadStream
 	int16		max_pinned_buffers;
 	int16		pinned_buffers;
 	int16		distance;
+	int16		sustain;
 	bool		advice_enabled;
 
 	/*
@@ -271,8 +272,11 @@ read_stream_start_pending_read(ReadStream *stream, bool suppress_advice)
 	if (!need_wait)
 	{
 		/* Look-ahead distance decays, no I/O necessary. */
-		if (stream->distance > 1)
+		if (stream->sustain > 0)
+			stream->sustain--;
+		else if (stream->distance > 1)
 			stream->distance--;
+		stream->seq_blocknum = InvalidBlockNumber;
 	}
 	else
 	{
@@ -680,6 +684,7 @@ read_stream_next_buffer(ReadStream *stream, void **per_buffer_data)
 			stream->next_io_index = stream->max_ios > 1 ? 1 : 0;
 			stream->ios_in_progress = 1;
 			stream->ios[0].buffer_index = oldest_buffer_index;
+			stream->seq_start = next_blocknum;
 			stream->seq_blocknum = next_blocknum + 1;
 		}
 		else
@@ -765,11 +770,18 @@ read_stream_next_buffer(ReadStream *stream, void **per_buffer_data)
 		if (stream->ios_in_progress == 0 ||
 			WaitReadBuffersMightStall(&stream->ios[a].op))
 		{
-			/* Boost: it's not OK if something as close as A might stall. */
+			/* Attack: it's not OK if something as close as A might stall. */
 			if (stream->distance > stream->max_pinned_buffers / 2)
 				stream->distance = stream->max_pinned_buffers;
 			else
 				stream->distance *= 2;
+
+			/*
+			 * Don't let interleaving hits decay the distance immediately, as
+			 * we might be sent back to 1 immediately.  We hold this value for
+			 * a while.
+			 */
+			stream->sustain = 16;
 		}
 		else if (a != b &&
 				 !WaitReadBuffersMightStall(&stream->ios[b].op))
@@ -843,6 +855,7 @@ read_stream_next_buffer(ReadStream *stream, void **per_buffer_data)
 	/* See if we can take the fast path for all-cached scans next time. */
 	if (stream->ios_in_progress == 0 &&
 		stream->pinned_buffers == 1 &&
+		stream->sustain == 0 &&
 		stream->distance == 1 &&
 		stream->pending_read_nblocks == 0 &&
 		stream->per_buffer_data_size == 0)
