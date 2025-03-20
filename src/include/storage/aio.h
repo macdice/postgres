3432ae5d2421ab49bce015c6a64bb256ae36625d
Andres Freund <andres@anarazel.de>
2025-03-20 11:32:10 -0400

    aio, bufmgr: Comment fixes
    Some of these comments have been wrong for a while (12f3867f5534), some I
    recently introduced (da7226993fd, 55b454d0e14). This also updates a comment in
    FlushBuffer(), which will be copied in a future commit.
    
    These changes seem big enough to be worth doing in separate commits.
    
    Suggested-by: Noah Misch <noah@leadboat.com>
    Discussion: https://postgr.es/m/20250319212530.80.nmisch@google.com

--- src/include/storage/aio.h
+++ src/include/storage/aio.h
@@ -72,7 +72,7 @@ typedef enum PgAioHandleFlags
 /*
  * The IO operations supported by the AIO subsystem.
  *
- * This could be in aio_internal.h, as it is not pubicly referenced, but
+ * This could be in aio_internal.h, as it is not publicly referenced, but
  * PgAioOpData currently *does* need to be public, therefore keeping this
  * public seems to make sense.
  */
