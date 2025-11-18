d23fb4d32eaa71ed3a0b8f0f2a7d27fec036b4ea
Thomas Munro <thomas.munro@gmail.com>
2025-11-19 00:00:13 +1300

    jit: API changes for LLVM 22.
    The lifetime.end intrinsic can now only be used for stack memory
    allocated with alloca.  We were using it to tell the optimizer that we
    are no longer interested in the arguments and null flag in a
    FunctionCallInfo struct, so it could avoid actually storing them if it
    managed to inline the function and keep everything in registers.  It
    can't figure that out by itself because it's part of the ExecEvalStep
    struct and we scribble on it directly rather than building a new one on
    the stack.
    
    Instead, store the special poison value (undef would work too).  This
    is a no-op, but tells the optimizer that we are not interested in the
    values.
    
    XXX Verify inlined results!
    
    Deform functions use LLVMBuildAlloca() for a stack variable, but that
    memory is reclaimed implicitly by the ret instruction.
    
    https://github.com/llvm/llvm-project/pull/149310
    https://llvm.org/docs/LangRef.html#llvm-lifetime-end-intrinsic
    https://llvm.org/docs/LangRef.html#i-alloca

--- src/backend/jit/llvm/llvmjit_expr.c
+++ src/backend/jit/llvm/llvmjit_expr.c
@@ -62,7 +62,9 @@ static LLVMValueRef build_EvalXFuncInt(LLVMBuilderRef b, LLVMModuleRef mod,
 									   LLVMValueRef v_state,
 									   ExprEvalStep *op,
 									   int natts, LLVMValueRef *v_args);
+#if LLVM_VERSION_MAJOR < 22
 static LLVMValueRef create_LifetimeEnd(LLVMModuleRef mod);
+#endif
 
 /* macro making it easier to call ExecEval* functions */
 #define build_EvalXFunc(b, mod, funcname, v_state, op, ...) \
@@ -3007,13 +3009,10 @@ BuildV1Call(LLVMJitContext *context, LLVMBuilderRef b,
 			LLVMModuleRef mod, FunctionCallInfo fcinfo,
 			LLVMValueRef *v_fcinfo_isnull)
 {
-	LLVMContextRef lc;
 	LLVMValueRef v_fn;
 	LLVMValueRef v_fcinfo_isnullp;
 	LLVMValueRef v_retval;
 	LLVMValueRef v_fcinfo;
-
-	lc = LLVMGetModuleContext(mod);
 
 	v_fn = llvm_function_reference(context, b, mod, fcinfo);
 
@@ -3030,11 +3029,21 @@ BuildV1Call(LLVMJitContext *context, LLVMBuilderRef b,
 	if (v_fcinfo_isnull)
 		*v_fcinfo_isnull = l_load(b, TypeStorageBool, v_fcinfo_isnullp, "");
 
+#if LLVM_VERSION_MAJOR >= 22
+	/*
+	 * Mark memory as unimportant, signaling that writes to memory don't to be
+	 * retained (important for inlining potential).
+	 */
+	for (int i = 0; i < fcinfo->nargs; ++i)
+		LLVMBuildStore(b, LLVMGetPoison(StructNullableDatum), l_funcvaluep(b, v_fcinfo, i));
+	LLVMBuildStore(b, LLVMGetPoison(TypeStorageBool), v_fcinfo_isnullp);
+#else
 	/*
 	 * Add lifetime-end annotation, signaling that writes to memory don't have
 	 * to be retained (important for inlining potential).
 	 */
 	{
+		LLVMContextRef lc = LLVMGetModuleContext(mod);
 		LLVMValueRef v_lifetime = create_LifetimeEnd(mod);
 		LLVMValueRef params[2];
 
@@ -3046,6 +3055,7 @@ BuildV1Call(LLVMJitContext *context, LLVMBuilderRef b,
 		params[1] = l_ptr_const(&fcinfo->isnull, l_ptr(LLVMInt8TypeInContext(lc)));
 		l_call(b, LLVMGetFunctionType(v_lifetime), v_lifetime, params, lengthof(params), "");
 	}
+#endif
 
 	return v_retval;
 }
@@ -3083,6 +3093,7 @@ build_EvalXFuncInt(LLVMBuilderRef b, LLVMModuleRef mod, const char *funcname,
 	return v_ret;
 }
 
+#if LLVM_VERSION_MAJOR < 22
 static LLVMValueRef
 create_LifetimeEnd(LLVMModuleRef mod)
 {
@@ -3112,3 +3123,4 @@ create_LifetimeEnd(LLVMModuleRef mod)
 
 	return fn;
 }
+#endif
