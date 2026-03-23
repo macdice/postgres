42f5da0f96a4856fdfc27a30335d75efb8a706da
Thomas Munro <thomas.munro@gmail.com>
2026-03-24 10:41:13 +1300

    pg_cpu: Portable CPU identifiers.
     * pg_cpu_t for CPU IDs, because it's not just an int on Windows
     * pg_cpu_current() for CPU the caller is running on right now
    
    The second thing only works on Linux, FreeBSD, Solaris/illumos, Cygwin
    and Windows, and returns 0 on other systems.  Patches for other systems
    are welcome.
    
    No users yet, but later patches use these.

--- src/include/port/pg_cpu.h
+++ src/include/port/pg_cpu.h
@@ -1,7 +1,7 @@
 /*-------------------------------------------------------------------------
  *
  * pg_cpu.h
- *	  Runtime CPU feature detection
+ *	  Identification of CPUs and runtime feature checks.
  *
  * Portions Copyright (c) 1996-2026, PostgreSQL Global Development Group
  * Portions Copyright (c) 1994, Regents of the University of California
@@ -12,6 +12,68 @@
  */
 #ifndef PG_CPU_H
 #define PG_CPU_H
+
+#ifdef HAVE_SCHED_GETCPU
+#include <sched.h>
+#endif
+#ifdef HAVE_GETCPUID
+#include <sys/processor.h>
+#endif
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+/* Type used to identify logical processors in system APIs. */
+#ifndef WIN32
+
+/* Unix systems refer to CPUs with a small zero-indexed integer. */
+typedef int pg_cpu_t;
+#define PG_CPU_FORMAT "%d"
+#define PG_CPU_FORMAT_ARG(x) x
+#define PG_CPU_NUMBER(x) x
+#define PG_CPU_GROUP(x) 0
+#define PG_CPU_AS_INT(x) x
+#define PG_CPU_HAS_GROUP false
+
+#else
+
+/*
+ * Windows APIs use a two-part identifier in the variants that support more
+ * than 64 logical CPUs.  The "Number" part is 0-63, and the "Group" part is
+ * 0-N.
+ */
+typedef PROCESSOR_NUMBER pg_cpu_t;
+#define PG_CPU_FORMAT "%d:%d"
+#define PG_CPU_FORMAT_ARG(x) (x).Group, (x).Number
+#define PG_CPU_NUMBER(x) ((x).Number)
+#define PG_CPU_GROUP(x) ((x).Group)
+#define PG_CPU_AS_INT(x) (((x).Group << 6) | (x).Number)
+#define PG_CPU_HAS_GROUP true
+
+#endif
+
+/*
+ * Return the CPU that the caller is running on as of this very instant.
+ * Returns 0 if we don't have support on this system.  This can't fail, and is
+ * expected to have a fast implementation that doesn't enter the kernel (user
+ * mode instructions, vDSO, thread local updated by kernel, etc).
+ */
+static inline pg_cpu_t
+pg_cpu_current(void)
+{
+#if defined(HAVE_SCHED_GETCPU)
+	return sched_getcpu();		/* Linux, FreeBSD */
+#elif defined(GETCPUID)
+	return getcpuid();			/* Solaris, illumos */
+#elif defined(WIN32)
+	pg_cpu_t	cpu;
+
+	GetCurrentProcessorNumberEx(&cpu);
+	return cpu;
+#else
+	return 0;
+#endif
+}
 
 #if defined(USE_SSE2) || defined(__i386__)
 
