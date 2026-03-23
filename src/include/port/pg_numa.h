79392bb27764aaab42d73cb895910e4dce638a79
Thomas Munro <thomas.munro@gmail.com>
2026-03-23 20:43:51 +1300

    pg_numa: Top-level CPU topology queries.
     * pg_numa_get_node_for_cpu()
     * pg_numa_get_current_node()
     * pg_numa_get_cpus_for_node()
    
    This adds limited FreeBSD and Windows support into pg_numa, but not
    yet the memory-related functions.
    
    No callers yet, but later patches would make use of these.

--- src/include/port/pg_numa.h
+++ src/include/port/pg_numa.h
@@ -14,9 +14,25 @@
 #ifndef PG_NUMA_H
 #define PG_NUMA_H
 
+#ifdef HAVE_GETCPU
+#include <sched.h>
+#endif
+#ifdef WIN32
+#include <windows.h>
+#endif
+
+#include "port/pg_cpu.h"
+#include "port/pg_cpuset.h"
+
+#if defined(USE_LIBNUMA) || defined(WIN32)
+#define PG_NUMA_HAVE_WORKING_GET_NODE_FOR_CPU
+#endif
+
 extern PGDLLIMPORT int pg_numa_init(void);
 extern PGDLLIMPORT int pg_numa_query_pages(int pid, unsigned long count, void **pages, int *status);
 extern PGDLLIMPORT int pg_numa_get_max_node(void);
+extern PGDLLIMPORT int pg_numa_get_node_for_cpu(pg_cpu_t cpu);
+extern PGDLLIMPORT int pg_numa_get_cpus_for_node(int node, pg_cpuset_t *cpuset);
 
 #ifdef USE_LIBNUMA
 
@@ -38,5 +54,31 @@ pg_numa_touch_mem_if_required(void *ptr)
 	do {} while(0)
 
 #endif
+
+/*
+ * Report which node the caller is currently running on.  This can't fail, and
+ * is expected to have a fast implementation that doesn't enter the kernel
+ * (user mode instructions, vDSO, thread local updated by kernel, etc).
+ */
+static inline int
+pg_numa_get_current_node(void)
+{
+#ifdef HAVE_GETCPU
+#define PG_NUMA_HAVE_WORKING_GET_CURRENT_NODE
+	unsigned int node;
+
+	/* The only specified error is EFAULT. */
+	getcpu(NULL, &node);
+	return node;
+#elif defined(WIN32)
+#define PG_NUMA_HAVE_WORKING_GET_CURRENT_NODE
+	pg_cpu_t	cpu = pg_cpu_current();
+	USHORT		node;
+
+	return GetNumaProcessorNodeEx(&cpu, &node) ? node : 0;
+#else
+	return 0;
+#endif
+}
 
 #endif							/* PG_NUMA_H */
